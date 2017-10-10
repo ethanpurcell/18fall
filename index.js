766d6ba7d1f20b034a497fac91e418a405e67ded
ethanpurcell <ethanpurcell@seattleacademy.org>
2017-10-10 13:49:48 -0700

    add bad word filter

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+  	if(msg.includes("poop")) msg="you said a baddy"
     io.emit('chat message', msg);
   });
 });
